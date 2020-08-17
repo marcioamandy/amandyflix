6f9eddeba3e3ca8238e03c981de04a938e923c86
Marcio Martins <marcioamandy@gmail.com>
2020-08-17 15:31:03 -0300

    Setup inicial do projeto amandyflix

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../assets/img/logo.png';
+import './Menu.css';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
+
+
+function Menu() {
+return (
+    <nav className="Menu">
+        <a href="/">
+            <img className="Logo" src={Logo} alt="Logo da AmandyFlix" />
+        </a>
+        <Button as="a" className="Button" href="/">
+            Novo vídeo
+        </Button>
+    </nav>
+    );
+}
+export default Menu
